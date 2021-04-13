a929a6b2e3fdeb833250ae3dc210819075b9893e
vladimirs-sapega <vladimirs_sapega@inbox.lv>
2021-04-14 02:11:34 +0300

    first commit

---
+++ src/main/java/JavaTestMaven.java
@@ -0,0 +1,11 @@
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+
+public class JavaTestMaven {
+
+        public static void main(String[] args) {
+            File tempDirectory = FileUtils.getTempDirectory();
+        }
+    }
+
